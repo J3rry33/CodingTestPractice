e7eb13973dcd10876c26084cf31f0641809a114e
J3rry33 <lancia01@naver.com>
2024-11-25 18:47:00 +0900

    숫자 문자열과 영단어

--- src/main/java/me/jungseung/Solution11_25_2.java
+++ src/main/java/me/jungseung/Solution11_25_2.java
@@ -1,4 +1,13 @@
 package me.jungseung;
 
 public class Solution11_25_2 {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while(n>=a){
+            answer += (n/a)*b;
+            n = (n/a)*b + n%a;
+        }
+        return answer;
+    }
 }
+
