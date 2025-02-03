2a10ced78d34d5bc488c0e83d265225ef94f2e4a
J3rry33 <lancia01@naver.com>
2025-02-03 21:56:20 +0900

    문자열 출력하기

---
+++ src/main/java/me/jungseung/Solution02_03_1.java
@@ -0,0 +1,10 @@
+package me.jungseung;
+import java.util.Scanner;
+
+public class Solution02_03_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
